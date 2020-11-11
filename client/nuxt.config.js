f6547199a2b8c84154ddb301175bc8b2fe733dfa
valera <inifrey@gmail.com>
2020-11-11 18:07:21 +0200

    Add NuxtJs

---
+++ client/nuxt.config.js
@@ -0,0 +1,89 @@
+import colors from 'vuetify/es5/util/colors'
+
+const { join } = require('path');
+const { copySync, removeSync } = require('fs-extra');
+
+export default {
+  srcDir: __dirname,
+
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    titleTemplate: '%s - Notes',
+    title: 'Notes',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+
+  hooks: {
+    generate: {
+      done (generator) {
+        // Copy dist files to public/_nuxt
+        if (generator.nuxt.options.dev === false && generator.nuxt.options.mode === 'spa') {
+          const publicDir = join(generator.nuxt.options.rootDir, 'public', '_nuxt')
+          removeSync(publicDir)
+          copySync(join(generator.nuxt.options.generate.dir, '_nuxt'), publicDir)
+          copySync(join(generator.nuxt.options.generate.dir, '200.html'), join(publicDir, 'index.html'))
+          removeSync(generator.nuxt.options.generate.dir)
+        }
+      }
+    }
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{path}', function () {
+    return file_get_contents(public_path('_nuxt/index.html'));
+})->where('path', '(.*)');
